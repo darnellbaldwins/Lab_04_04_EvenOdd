c4b7545924220e4edc248dd9509834a071a59156
Darnell Baldwin <darnellbaldwin@Darnells-MacBook-Air.local>
2024-09-30 20:37:13 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int evenOddCalculator = 4;
+        if (evenOddCalculator % 2 == 0) {
+            System.out.println(evenOddCalculator + " " + "Is Even");
+        } else {
+            System.out.println(evenOddCalculator + " " + "Is Odd");
+        }
+    }
+}
